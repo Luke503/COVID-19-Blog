dc27fae8457e004763631f40054dde2a7c3a0ae1
Zachary Smith <zackmsmith10@gmail.com>
2020-12-10 01:37:25 -0800

    handlebars templates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: null }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
@@ -13,26 +13,22 @@ app.get('/', function(req, res) {
   res.status(200).render('postPage', postData);
 });
 */
+
+/*
 app.get('/index', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/public/index.html');
 });
-
+*/
 app.use(express.json());
 
 app.use(express.static('public'));
 
-
-app.post('/', function (req, res, next) {
-  console.log("== req.body:", req.body);
-  if (req.body && req.body.firstname && req.body.lastname) {
-    var person = req.params.person.toLowerCase();
-    if (peopleData[person]) {
-      // Add photo to DB...
-      peopleData[person].photos.push({
-        caption: req.body.caption,
-        url: req.body.url
-      });
-
+/*
+app.get('/', function (req, res, next) {
+  console.log("Using handlebars");
+  res.status(200).render('main');
+});
+*/
 app.get('*', function(req, res) {
     res.status(404).render('404', {});
 });
