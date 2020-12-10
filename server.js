6827fe14782cfd9962c1d2cc864ebe3be084192e
Zachary Smith <zackmsmith10@gmail.com>
2020-12-10 01:10:00 -0800

    server and main.handlebars

--- server.js
+++ server.js
@@ -17,7 +17,21 @@ app.get('/index', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.json());
+
 app.use(express.static('public'));
+
+
+app.post('/', function (req, res, next) {
+  console.log("== req.body:", req.body);
+  if (req.body && req.body.firstname && req.body.lastname) {
+    var person = req.params.person.toLowerCase();
+    if (peopleData[person]) {
+      // Add photo to DB...
+      peopleData[person].photos.push({
+        caption: req.body.caption,
+        url: req.body.url
+      });
 
 app.get('*', function(req, res) {
     res.status(404).render('404', {});
